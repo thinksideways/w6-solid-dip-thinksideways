8ebab73da611d118dd6e1d811343e025e4238ad9
Sam Kaiser <samuel.k.public@gmail.com>
2025-10-03 18:33:38 -0500

    Clean up potential lower level dependencies on CharacterBase class

--- Models/CharacterBase.cs
+++ Models/CharacterBase.cs
@@ -1,6 +1,4 @@
-﻿using W6_assignment_template.Interfaces;
-
-namespace W6_assignment_template.Models
+﻿namespace W6_assignment_template.Models
 {
     public abstract class CharacterBase : ICharacter
     {
@@ -19,32 +17,10 @@ namespace W6_assignment_template.Models
 
         protected CharacterBase() { }
 
-        public void Attack(ICharacter target)
-        {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"{Name} attacks {target.Name}");
-            Console.ResetColor();
+        public abstract void Attack(ICharacter target);
 
-            if (this is Player player && target is ILootable targetWithTreasure && !string.IsNullOrEmpty(targetWithTreasure.Treasure))
-            {
-                Console.WriteLine($"{Name} takes {targetWithTreasure.Treasure} from {target.Name}");
-                player.Gold += 10; // Assuming each treasure is worth 10 gold
-                targetWithTreasure.Treasure = null; // Treasure is taken
-            }
-            else if (this is Player playerWithGold && target is Player targetWithGold && targetWithGold.Gold > 0)
-            {
-                Console.WriteLine($"{Name} takes gold from {target.Name}");
-                playerWithGold.Gold += targetWithGold.Gold;
-                targetWithGold.Gold = 0; // Gold is taken
-            }
-        }
+        public abstract void Move();
 
-        public void Move()
-        {
-            Console.WriteLine($"{Name} moves.");
-        }
-
-        // Abstract method for unique behavior to be implemented by derived classes
         public abstract void UniqueBehavior();
     }
 }
